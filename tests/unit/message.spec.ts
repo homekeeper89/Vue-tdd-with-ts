799fad80bbaf30f87de5a54de6b37e58defae373
matthew <homekeeper89@gmail.com>
2019-12-30 01:18:23 +0900

    [fix] lint 적용

--- tests/unit/message.spec.ts
+++ tests/unit/message.spec.ts
@@ -1,22 +1,23 @@
-import {shallowMount} from '@vue/test-utils'
-import message from '@/components/message.vue'
+import {shallowMount} from '@vue/test-utils';
+import message from '@/components/message.vue';
 
-describe('hello Message', ()=>{
-    const build = ()=>{
-        const wrapper = shallowMount(message)
+describe('hello Message', () => {
+    const build = () => {
+        const wrapper = shallowMount(message);
         return {
-            wrapper
-        }
-    }
+            wrapper,
+        };
+    };
 
-    it('test render well', ()=>{
-        const {wrapper} = build()
-        expect(wrapper.exists()).toBe(true)
-    })
-    it('test input field', ()=>{
-        const {wrapper} = build()
-        const expected = "helloWorld"
-        wrapper.find("[message-msg]").setValue(expected)
-        expect(wrapper.find(".message-contents").text()).toBe(expected)
-    })
-})
+    it('test render well', () => {
+        const {wrapper} = build();
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('test input field', () => {
+        const {wrapper} = build();
+        const expected = 'helloWorld';
+        wrapper.find('[message-msg]').setValue(expected);
+        expect(wrapper.find('.message-contents').text()).toBe(expected);
+    });
+});
