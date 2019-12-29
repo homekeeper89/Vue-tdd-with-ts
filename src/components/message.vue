a9e522d42abd476f72dd7ddc063c4940db61d734
matthew <homekeeper89@gmail.com>
2019-12-29 23:18:19 +0900

    [new] message input component 추가
    home 화면 변경 > @Component 사용

---
+++ src/components/message.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" v-model="message">
+    <div>{{message}}</div>
+  </div>
+</template>
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+@Component
+export default class Message extends Vue{
+  message: string = 'Please text some message'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <message></message>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import message from '@/components/message.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+@Component({
+  components:{
+    message,
   }
-}
+})
+export default class Home extends Vue{}
 </script>
