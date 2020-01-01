a1c27cc260a43caf0fa8b2ddfbe71d5b3340c0a0
matthew <homekeeper89@gmail.com>
2020-01-01 16:30:40 +0900

    [new] message 보여주는 박스 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <my-checkbox v-model="checked" @myCustomClick="change"></my-checkbox>
+    <div>
+      {{text}}
+    </div>
   </div>
 </template>
 
@@ -8,14 +12,22 @@
 import { Component, Vue } from "vue-property-decorator";
 import message from "@/components/message.vue";
 import children from "@/components/children.vue";
+import myCheckbox from '@/components/my-checkbox.vue';
 
 @Component({
   components: {
     message,
-    children
+    children,
+    myCheckbox
   }
 })
 export default class Home extends Vue {
-  
+  checked:boolean = false;
+  text:string = "동의하지 않습니다."
+
+  change(checked:boolean){
+    this.checked = checked;
+    this.text = checked ? "동의합니다" : "동의하지 않습니다."
+  }
 }
 </script>
