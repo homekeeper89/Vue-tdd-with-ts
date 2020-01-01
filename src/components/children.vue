a42f876296b151cc268130b70916ad9e63db3e22
matthew <homekeeper89@gmail.com>
2020-01-01 14:47:04 +0900

    [new] provide / inject 추가
    props처럼 데이터를 전달해주지만 직계가 아닌 일반도 가능함

--- src/components/children.vue
+++ src/components/children.vue
@@ -1,36 +1,15 @@
 <template>
   <div>
-    <div class="children-info">
-      {{parentMessge}}
-    </div>
-    <div class="children-alert">
-      {{alertMessage}}
-    </div>
-    <hr>
-    <div class="children-btn">
-      <button class="children-btn-counter" @click = "clickCount">부모 숫자 증가시키는 버튼 </button>
-    </div>
+    {{message}}
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue, Prop, Watch, Emit} from 'vue-property-decorator';
+import { Component, Vue, Prop, Watch, Emit, Inject} from 'vue-property-decorator';
 
 @Component
 export default class Children extends Vue {
-  @Prop() public parentMessge?: string;
-  alertMessage: string = '';
-  checkCount: number = 0;
-  //immedate true : 해당 속성 현재값 바로 호출, deep 은 안에 객체 변화까지 감시
-  // {immediate: true, deep:true}
-  @Watch('parentMessge')
-  update(value:string, oldValue:string){
-    this.alertMessage = 'message updated'
-  }
-  
-  @Emit('counter')
-  clickCount(){
-    console.log('자식임')
-    this.checkCount += 20;
-  }
+  // ! 붙이면 초기화 하지 않는 것이 의도화 되었다.
+  // 부모로부터 받아오는 것이므로 변경되선 안된다  = readonly
+  @Inject() readonly message!: string;
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <hr>
-    <message></message>
-    <hr>
-    <button class="home-btn" @click.prevent="changeMessage">메세지 변경</button>
-    <children :parentMessge="message" @counter="counterss"></children>
-    <div class="home-message">부모에서 숫자를 보여줌 : {{count}}</div>
+    <children></children>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Provide } from "vue-property-decorator";
 import message from "@/components/message.vue";
 import children from "@/components/children.vue";
 
@@ -22,15 +16,6 @@ import children from "@/components/children.vue";
   }
 })
 export default class Home extends Vue {
-  public message: string = "hello World";
-  count: number = 0;
-
-  changeMessage() {
-    this.message = "change";
-  }
-
-  counterss(){
-    this.count++;
-  }
+  @Provide('message') msg: string = ""
 }
 </script>
