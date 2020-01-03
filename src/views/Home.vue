e1318811fd1aab2a1ae53bf3f8e6407390daa549
matthew <homekeeper89@gmail.com>
2020-01-03 23:09:41 +0900

    [new] mixin 사용법 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,12 @@
 import { Component, Vue } from "vue-property-decorator";
 import message from "@/components/message.vue";
 import children from "@/components/children.vue";
+import practiceMixin from '@/components/practiceMixin.vue';
 
 @Component({
   components: {
     message,
-    children
+    children,
   }
 })
 export default class Home extends Vue {
