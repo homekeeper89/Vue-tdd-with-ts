8c9dde98f21f90bf2c18b3e579b67ed582ccf8b9
matthew <homekeeper89@gmail.com>
2020-01-04 15:00:07 +0900

    [new] vuex 사용법 간단하게 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,30 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { StoreOptions, ActionContext} from 'vuex';
 
 Vue.use(Vuex);
+interface State {
+  count: number;
+}
+const store:StoreOptions<State> = {
+  state:{
+    count: 0
+  },
+  mutations:{
+    setCount(state:State, count:number){
+      state.count = count;
+    }
+  },
+  actions:{
+    increaseCount({state,commit}:ActionContext<State, State>){
+      commit('setCount', state.count +1);
+    },
+    decreaseCount({state,commit}:ActionContext<State, State>){
+      commit('setCount', state.count -1);
+    }
+  },
+  getters:{
+    count: (state:State) => state.count
+  }
+}
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+export default new Vuex.Store(store);
