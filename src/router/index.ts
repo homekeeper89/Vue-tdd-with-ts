071af874ed4020e35d744743af281cb0072cb1ae
matthew <homekeeper89@gmail.com>
2020-01-06 01:18:21 +0900

    [new] item-list route 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import itemList from '@/views/item-list.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  
+  {
+    path:'/:status?',
+    name:'item-list',
+    component: itemList
+  }
 ];
 
 const router = new VueRouter({
